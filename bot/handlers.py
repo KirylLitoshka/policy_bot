b4db6fa4ae18f3a6e70c448a6ca71474d1952044
Kirill Litoshko <kovzik08@gmail.com>
2022-12-14 17:01:34 +0300

    updated project

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from bot.profiles import Profile
+from bot.profiles import Profile, User
 from bot.utils import build_policy, build_terms_of_use
 
 ACTIVITY_FIELDS = [
@@ -16,57 +16,6 @@ APPLICATION_TYPES = [
     "Gamedev",
     "Работаем со всеми приложениями которые видим"
 ]
-
-
-async def create_privacy_policy(message, state):
-    async with state.proxy() as data:
-        if "name" not in data:
-            data["document_type"] = "policy"
-            return await set_user_name(message)
-        user_data = data.as_dict()
-        if user_data.get("policy_link"):
-            return await message.answer(user_data['policy_link'])
-        policy_link = await build_policy(user_data)
-        data["policy_link"] = policy_link
-    await message.answer(policy_link)
-    if not user_data.get("terms_link"):
-        await asyncio.sleep(2)
-        keyboard = types.InlineKeyboardMarkup(
-            inline_keyboard=[[
-                types.InlineKeyboardButton("Да", callback_data="terms"),
-                types.InlineKeyboardButton("Нет", callback_data="finish")
-            ]]
-        )
-        return await message.answer("Желаете создать Terms of Use?", reply_markup=keyboard)
-    await finish(message, state)
-
-
-async def create_terms_of_use(message, state):
-    async with state.proxy() as data:
-        if "name" not in data:
-            data["document_type"] = "terms"
-            return await set_user_name(message)
-        user_data = data.as_dict()
-        if user_data.get("terms_link"):
-            return await message.answer(user_data['terms_link'])
-        terms_link = await build_terms_of_use(user_data)
-        data["terms_link"] = terms_link
-    await message.answer(terms_link)
-    if not user_data.get("policy_link"):
-        await asyncio.sleep(2)
-        keyboard = types.InlineKeyboardMarkup(
-            inline_keyboard=[[
-                types.InlineKeyboardButton("Да", callback_data="privacy"),
-                types.InlineKeyboardButton("Нет", callback_data="finish")
-            ]]
-        )
-        return await message.answer("Желаете создать Privacy Policy?", reply_markup=keyboard)
-    await finish(message, state)
-
-
-async def set_user_name(message):
-    await Profile.name.set()
-    await message.answer("Введите ваше имя:", reply_markup=types.ReplyKeyboardRemove())
 
 
 async def process_activity_field(message: types.Message, state: FSMContext):
@@ -114,11 +63,59 @@ async def process_promo_code(message: types.Message, state: FSMContext):
     await state.set_state("*")
 
 
+async def create_privacy_policy(message, state):
+    current_state = await state.get_state()
+    if current_state != "*":
+        return await message.delete()
+    async with state.proxy() as data:
+        if "name" not in data:
+            data["document_type"] = "policy"
+            return await set_user_name(message)
+        user_data = data.as_dict()
+        del data["name"]
+    policy_link = await build_policy(user_data)
+    await message.answer(policy_link)
+    await asyncio.sleep(2)
+    keyboard = types.InlineKeyboardMarkup(
+        inline_keyboard=[[
+            types.InlineKeyboardButton("Да", callback_data="terms"),
+            types.InlineKeyboardButton("Нет", callback_data="finish")
+        ]]
+    )
+    return await message.answer("Желаете создать Terms of Use?", reply_markup=keyboard)
+
+
+async def create_terms_of_use(message, state):
+    current_state = await state.get_state()
+    if current_state != "*":
+        return await message.delete()
+    async with state.proxy() as data:
+        if "name" not in data:
+            data["document_type"] = "terms"
+            return await set_user_name(message)
+        user_data = data.as_dict()
+        del data["name"]
+    terms_link = await build_terms_of_use(user_data)
+    await message.answer(terms_link)
+    await asyncio.sleep(2)
+    keyboard = types.InlineKeyboardMarkup(
+        inline_keyboard=[[
+            types.InlineKeyboardButton("Да", callback_data="privacy"),
+            types.InlineKeyboardButton("Нет", callback_data="finish")
+        ]]
+    )
+    return await message.answer("Желаете создать Privacy Policy?", reply_markup=keyboard)
+
+
+async def set_user_name(message):
+    await User.name.set()
+    await message.answer("Введите ваше имя:", reply_markup=types.ReplyKeyboardRemove())
+
+
 async def process_username(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data["name"] = message.text
-
-    await Profile.next()
+    await User.next()
     await asyncio.sleep(1)
     await message.answer("Введите название приложения:")
 
@@ -126,7 +123,7 @@ async def process_username(message: types.Message, state: FSMContext):
 async def process_application_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data["application_name"] = message.text
-    await Profile.next()
+    await User.next()
     await asyncio.sleep(1)
     await message.answer("Введите ваш email:")
 
@@ -161,21 +158,13 @@ async def create_by_callback_data(query, state):
 
 
 async def finish(query, state):
-    # Дописать сохранение состояния FSM если требуется
-    final_message = "Сообщение при выходе"
+    await state.set_state("*")
+    final_message = """
+        и в сообщении в конце мы говорим "Ваш документ готов. Вы можете создать и другие документы:
+        ✔️Privacy Policy при помощи команды /privacy
+        ✔️Terms of use при помощи команды /terms
+    """
     if isinstance(query, types.CallbackQuery):
         await query.message.answer(final_message)
     else:
         await query.answer(final_message)
-
-
-async def cmd_start(message: types.Message):
-    await message.answer("Здравствуйте! Для старта работы просим пройти анкетирование")
-    await asyncio.sleep(1)
-    await Profile.activity_field.set()
-    keyboard = types.ReplyKeyboardMarkup(
-        resize_keyboard=True,
-        one_time_keyboard=True,
-        keyboard=[[types.KeyboardButton(text=field)] for field in ACTIVITY_FIELDS]
-    )
-    await message.answer("Ваша сфера деятельности:", reply_markup=keyboard)
